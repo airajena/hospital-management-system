5daec395dd8c8d9da9c48b78d938d0a6904ac87b
Aira Jena <airajena.hack@gmail.com>
2025-01-23 11:36:31 +0530

    feat: Add appointment model

---
+++ src/main/java/com/hms/hms/models/Appointment.java
@@ -0,0 +1,25 @@
+package com.hms.hms.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long patientId;
+    private Long doctorId;
+    private String date;
+    private String time;
+    private String status;
+
+}
